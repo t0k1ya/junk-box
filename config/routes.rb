ab19e9a53ebd9462cf7b01796889909a21f2e74e
b07nji <b07ji.morelia@gmail.com>
2021-01-02 21:33:47 +0900

    記事投稿画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     get '/login', to: 'sessions#new'
     post '/login', to: 'sessions#create'
     get '/:id', to: 'admins#show'
+
+    resources :articles
   end
   # 例外処理 TODO: コメアウト取り消す
   # get '*path', controller: 'application', action: 'render_404'
