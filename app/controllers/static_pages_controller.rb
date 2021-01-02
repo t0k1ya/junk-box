2eccef7bcf6f0a55671cb900353e1078cad246fe
b07nji <b07ji.morelia@gmail.com>
2021-01-02 15:14:33 +0900

    記事詳細画面追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,6 @@ class StaticPagesController < ApplicationController
   def index
     contents = []
     CSV.foreach("#{Rails.root}/app/controllers/test.csv") do |row|
-      contents.push(row)
       (0..10).each {|n|
         contents.push(row)
       }
