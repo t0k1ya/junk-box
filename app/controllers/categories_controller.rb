a8c3a9f38f14d35d669afdb92d2a27f2ab246687
Tokiya Akiyhoshi <tokiyaakiyhoshi@MobilusnoMacBook-Pro.local>
2021-03-12 11:38:41 +0900

    ヘルパー読み込み

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,12 @@
 class CategoriesController < ApplicationController
-
+  include Admin::AdminsHelper
+  
   def show
     if tag = Category.find_by(name: params[:id])
       @articles = tag.articles
-      @tag_name = view_context.show_tag(tag.name)
+      @tag_name = show_tag(tag.name)
     end
       render 'static_pages/index'
   end
-  
+
 end
